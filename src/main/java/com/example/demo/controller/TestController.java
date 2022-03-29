2dccce34a0b3d53efa7572b6c23e057ffad0248a
zhanghl <610089335@qq.com>
2022-03-29 09:50:08 +0800

    测试 配置文件

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -5,37 +5,41 @@ import com.alibaba.fastjson.JSON;
 import com.example.demo.common.annotations.PassToken;
 import com.example.demo.common.annotations.UseToken;
 import com.example.demo.common.result.ApiResult;
+import com.example.demo.config.DeveloperProperty;
 import com.example.demo.domin.TbUser;
 import com.example.demo.request.dto.LoginDto;
 import com.example.demo.request.dto.UserAddDto;
 import com.example.demo.service.UserService;
 import com.example.demo.service.design.strategy.TestStrategyFactory;
-import com.example.demo.utils.*;
+import com.example.demo.utils.RedisUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * 用@RequiredArgsConstructor代替@Autowired
+ * 我们都知道注入一个 bean 有三种方式哦（set 注入, 构造器注入, 注解注入），Spring 推荐我们使用构造器的方式注入 Bean。
+ *
+ *
+ *
+ */
+@RequiredArgsConstructor
 @CrossOrigin
 @Slf4j
 @RestController
 @RequestMapping("/api")
 public class TestController {
 
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private RedisUtil redisUtil;
-    @Autowired
-    private RedissonClient redissonClient;
-    @Autowired
-    private TestStrategyFactory factory;
+    private final UserService userService;
+    private final RedisUtil redisUtil;
+    private final RedissonClient redissonClient;
+    private final TestStrategyFactory factory;
+    private final DeveloperProperty developerProperty;
 
     @UseToken
     @PassToken
@@ -57,7 +61,8 @@ public class TestController {
     @GetMapping("/3")
     public ApiResult<Object> testStrategy(@RequestParam("id") int id) throws Exception {
         String s = factory.getHandler(id).doSomeThing(11111);
-        return ApiResult.SUCCESS(s);
+        String message = developerProperty.getName() + "-" + developerProperty.getPhoneNumber();
+        return ApiResult.SUCCESS("message" + message + "-s:" + s);
     }
 
 
@@ -80,7 +85,7 @@ public class TestController {
         RBucket<String> keyObj = redissonClient.getBucket("redis01");
         String data = keyObj.get();
 
-        RBucket<String> keyObj1= redissonClient.getBucket("redisson01");
+        RBucket<String> keyObj1 = redissonClient.getBucket("redisson01");
         String data1 = keyObj1.get();
 
 //        RLock lock = redissonClient.getLock(id);
@@ -91,7 +96,7 @@ public class TestController {
 //        } finally {
 //            lock.unlock();
 //        }
-        return ApiResult.SUCCESS(data+"--"+data1);
+        return ApiResult.SUCCESS(data + "--" + data1);
     }
 
 
@@ -105,7 +110,6 @@ public class TestController {
     }
 }
 /**
- *
  * SETBIT key offset value
  * - value oloy 0 or 1
  * bitmap有自己的一套命令。可以把bitmap想象成一个以bit为单位的数组，数组的每个单元存储0和1，数组的下标叫做偏移量。
